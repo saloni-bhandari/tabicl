599fbed025aa0e472d2679c8fdf2816a4e253f5a
dillonloh <loh.dillon@gmail.com>
2026-03-07 02:26:51 +0000

    refactor: moving parameters

--- model/icl.py
+++ model/icl.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 
 class ICLearning(nn.Module):
-    def __init__(self, embedding_dim=32, test_size=1, nhead=1, vocab_size=100):
+    def __init__(self, embedding_dim=32, nhead=1, vocab_size=100):
         super(ICLearning, self).__init__()
         
         self.embedding = nn.Embedding(vocab_size, embedding_dim)
@@ -18,9 +18,7 @@ class ICLearning(nn.Module):
         )
         self.softmax = nn.Softmax(dim=-1) # softmax across the vocab
         
-        self.test_size = test_size
-
-    def forward(self, cls_outputs, y):
+    def forward(self, cls_outputs, y, test_size):
         
         batch_size, num_rows = y.size()
         print(f"Y shape: {y.shape}")
@@ -29,10 +27,12 @@ class ICLearning(nn.Module):
         y = self.embedding(y)
         print(f"Embedded Y shape: {y.shape}")
 
-        # mask the last self.test_size labels
-        y[:, -self.test_size: , :] = 0
+        # mask the last test_size labels
+        y[:, -test_size: , :] = 0
 
         # get representation that includes label (training samples + test samples)
+
+        print(cls_outputs.shape, y.shape)
         rep = cls_outputs + y 
         print(f"Representations of each row of data: {rep.shape}") # 2 batches, 4 rows each, 32 features in total (from the CLS tokens after row interaction + label embedding)
         # rep.shape=(2, 4, 32) => can consider that we have 2 sequences, of 4 tokens each, each token has 32 dim embedding
@@ -42,14 +42,13 @@ class ICLearning(nn.Module):
         # row 3 attends to 1 and 2 and 3, row 4 attends to 1 and 2 and 4
         mask = torch.zeros(num_rows, num_rows)
         
-
-        for i in range(num_rows - self.test_size):
-            mask[i, -self.test_size: ] = float('-inf')
+        for i in range(num_rows - test_size):
+            mask[i, -test_size: ] = float('-inf')
         
-        mask[-self.test_size: , -self.test_size: ] = float('-inf')
+        mask[-test_size: , -test_size: ] = float('-inf')
         
-        for i in range(self.test_size):
-            mask[-self.test_size+i, -self.test_size+i] = 0.0
+        for i in range(test_size):
+            mask[-test_size+i, -test_size+i] = 0.0
         
         print(f"Mask: {mask}")
         print(f"Mask shape: {mask.shape}")
@@ -59,7 +58,7 @@ class ICLearning(nn.Module):
 
         print(f"Representations after transformer {rep.shape}")
 
-        test_reps = rep[:, -self.test_size:, :]
+        test_reps = rep[:, -test_size:, :]
 
         print(f"Test representations: {test_reps.shape}")
 
@@ -79,6 +78,6 @@ if __name__ == "__main__":
 
     cls_outputs = torch.randn(batch_size, num_rows, embedding_dim)
     y = torch.randint(0, vocab_size, (batch_size, num_rows))
-    model = ICLearning(embedding_dim=embedding_dim, test_size=test_size, vocab_size=vocab_size)
+    model = ICLearning(embedding_dim=embedding_dim, vocab_size=vocab_size)
 
-    outputs = model(cls_outputs, y)
+    outputs = model(cls_outputs, y, test_size)
